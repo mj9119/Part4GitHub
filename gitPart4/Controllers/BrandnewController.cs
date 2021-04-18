03206b47300d04dbc88f3d34ab5ef7dbdd47dfc6
mj9119 <mj9119@sunflower.com>
2021-04-18 18:43:13 -0500

    Added BrandnewController

---
+++ gitPart4/Controllers/BrandnewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gitPart4.Controllers
+{
+    public class BrandnewController : Controller
+    {
+        // GET: Brandnew
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
